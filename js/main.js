0142c5852e4c2885ce64297bb58271cb95bc1ea9
Bryce Woodland <bwoodland@vivacitytech.com>
2025-08-12 23:32:29 -0600

    Fixed Hamburger Menu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-const hamburgerButton = document.querySelector('.hamburger-menu');
-const mobileNav = document.querySelector('#mobile-nav');
+// Copyright Date
+const currentYear = new Date().getFullYear();
+const dateClass = document.querySelector('.date');
+dateClass.textContent = currentYear;
 
-hamburgerButton.addEventListener('click', () => {
-    mobileNav.classList.toggle('main-nav--active');
-    hamburgerButton.classList.toggle('hamburger-menu--active');
-    document.body.classList.toggle('no-scroll');
+// Hamburger Menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mainNav = document.querySelector('.main-nav');
+const navLinks = document.querySelectorAll('a');
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('open');
+  mainNav.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
 });
 
-mobileNav.querySelectorAll('a').forEach((link) => {
-    link.addEventListener('click', () => {
-        mobileNav.classList.remove('main-nav--active');
-        hamburgerButton.classList.remove('hamburger-menu--active');
-        document.body.classList.remove('no-scroll');
-    });
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburgerMenu.classList.remove('open');
+    mainNav.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
 });
